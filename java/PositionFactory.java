12410c315e81377f4429e3b22b5fd9f642ffd177
paithanq@gmail.com <paithanq@gmail.com>
2018-03-30 08:47:00 -0400

    Updated Javadoc

--- java/PositionFactory.java
+++ java/PositionFactory.java
@@ -1,12 +1,17 @@
 /**
  * This is the interface for objects that create positions for a ruleset.
+ *
  * @author Kyle Burke <paithanq@gmail.com>
  */
-
-//import java.xxxxx.*;
 public interface PositionFactory<Game extends CombinatorialGame> {
 	
 	//public methods
+	
+	/**
+	 * Creates a new position.
+	 *
+	 * @return  Returns a position of Game.
+	 */
 	public Game getPosition();
 
 } //end of PositionFactory<Game extends CombinatorialGame>
